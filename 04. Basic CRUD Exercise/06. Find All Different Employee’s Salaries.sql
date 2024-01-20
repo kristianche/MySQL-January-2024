033bba49e1d8e68d426f7610dc863e1c616eb240
kristianche <chelebiev777@gmail.com>
2024-01-20 18:54:27 +0200

    Added task for Basic CRUD Exercise

---
+++ 04. Basic CRUD Exercise/06. Find All Different Employee’s Salaries.sql
@@ -0,0 +1,1 @@
+SELECT salary FROM employees;
---
+++ 04. Basic CRUD Exercise/09. Find Names of All Employees.sql
@@ -0,0 +1,4 @@
+ SELECT 
+	concat_ws(' ', first_name, middle_name, last_name) AS 'Full Name'
+FROM employees
+WHERE salary = 25000 OR salary = 14000 OR salary = 12500 OR salary = 23600;
