b53a99a17a62bebd85563579fae5fbf6d2cb31df
Kristian Chelebiev <114020925+kristianche@users.noreply.github.com>
2024-11-11 18:12:21 +0200

    Update 10. Extract average salary.sql

--- 15. Exam/10. Extract average salary.sql
+++ 15. Exam/10. Extract average salary.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION udf_average_salary_by_position_name (name VARCHAR(40)) RETURNS DECIMAL(10, 2)
 DETERMINISTIC
 BEGIN
-	DECLARE result DECIMAL(10, 2);
+    DECLARE result DECIMAL(10, 2);
     SET result := (SELECT
 					AVG(salary)
 				FROM workers w
